cfb15071f02af5824d22179dbda1e2cfc5a90f89
akashk6697 <akashk6697@gmail.com>
2024-12-24 19:18:15 +0530

    add matrix programs

---
+++ Matrix/MatrixRotateBy90.java
@@ -0,0 +1,50 @@
+
+// Java program to rotate a matrix by 90 degrees anti-
+// clockwise by reversing Rows and then Transposing
+import java.util.*;
+
+public class MatrixRotateBy90  {
+   public static void rotateMatrix(int[][] mat) {
+        int n = mat.length;
+        
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            int start = 0, end = n - 1;
+            while (start < end) {
+                int temp = mat[i][start];
+                mat[i][start] = mat[i][end];
+                mat[i][end] = temp;
+                start++;
+                end--;
+            }
+        }
+
+        // Performing Transpose
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] mat = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15, 16}
+        };
+
+        rotateMatrix(mat);
+
+        // Print the rotated matrix
+        for (int[] row : mat) {
+            for (int x : row) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+    }
+}
