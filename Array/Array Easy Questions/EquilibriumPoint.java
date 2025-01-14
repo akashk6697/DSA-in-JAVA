cc4a6ce0662fe12be7bc45cb2e8bdabbeb9ff138
akashk6697 <akashk6697@gmail.com>
2025-01-14 22:09:40 +0530

    add array program

--- Array/Array Easy Questions/EquilibriumPoint.java
+++ Array/Array Easy Questions/EquilibriumPoint.java
@@ -1,7 +1,9 @@
 
-
+// 1 [Naive Approach] Using Nested Loop – O(n^2) Time and O(1) Space
+// 2 [Better Approach] Prefix Sum and Suffix Sum Array – O(n) Time and O(n) Space
+// 3 [Expected Approach] Running Prefix Sum and Suffix Sum – O(n) Time and O(1) Space
 public class EquilibriumPoint {
-    
+    // Naive Approach: Using Nested Loop – O(n^2) Time and O(1) Space
     static int equilibrium(int arr[], int n)
     {
         int sum = 0; // initialize sum of whole array
@@ -22,7 +24,30 @@ public class EquilibriumPoint {
         }
         return -1;
     }
- 
+
+    /// Prefix Sum and Suffix Sum Array – O(n) Time and O(n) Space
+    static int equilibriumPoint(int[] arr) {
+        int prefSum = 0, total = 0;
+
+        // Calculate the array sum
+        for (int ele : arr) {
+            total += ele;
+        }
+
+        // Iterate pivot over all the elements of the array
+        for (int pivot = 0; pivot < arr.length; pivot++) {
+            int suffSum = total - prefSum - arr[pivot];
+            if (prefSum == suffSum) {
+                return pivot;
+            }
+            prefSum += arr[pivot];
+        }
+
+        // There is no equilibrium point
+        return -1;
+    }
+
+
     public static void main(String[] args)
     {
     
@@ -33,6 +58,9 @@ public class EquilibriumPoint {
         System.out.println("First Point of equilibrium "
         + "is at index "
         + equilibrium(arr, arr_size));
+        System.out.println("First Point of equilibrium "
+        + "is at index "
+        + equilibriumPoint(arr));
     }
 }
 
